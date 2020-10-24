a684b9caf0b2a76343f3be2c63bddb537f79e299
Thomas Eitler <thomas.eitler@pm.me>
2020-10-24 20:26:14 +0200

    add saascat command for random cat pics/gifs

---
+++ commands/saascat.go
@@ -0,0 +1,57 @@
+package saascat
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chat-bot/bot"
+	"github.com/joho/godotenv"
+	"gitlab.com/thomaseitler/juicybotv2.git/web"
+)
+
+type theCatAPIResponse []struct {
+	ID         string        `json:"id"`
+	URL        string        `json:"url"`
+	Breeds     []interface{} `json:"breeds"`
+	Categories []interface{} `json:"categories"`
+}
+
+var (
+	baseurl = "https://api.thecatapi.com/v1"
+	apikey  = os.Getenv("SAASCATAPIKEY")
+)
+
+func saascat(command *bot.Cmd) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	url := fmt.Sprintf("%s/images/search", baseurl)
+	var cat theCatAPIResponse
+
+	resp, err := web.MakeAPIRequest(url)
+	if err != nil {
+		return "", err
+	}
+	parseAPIResponse(resp, &cat)
+	return cat[0].URL, nil
+}
+
+func init() {
+	bot.RegisterCommand(
+		"cat", // command
+		"Returns a random cat pic or gif",
+		"!cat",
+		saascat) // function
+}
+
+func parseAPIResponse(r *http.Response, v *theCatAPIResponse) error {
+	err := json.NewDecoder(r.Body).Decode(&v)
+	if err != nil {
+		return fmt.Errorf("json decode error: %v", err)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/go-chat-bot/bot v0.0.0-20201004141219-763f9eeac7d5
 	github.com/go-chat-bot/plugins v0.0.0-20181006134258-491b3f9878d6 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/whyrusleeping/hellabot v0.0.0-20200821141615-1382ae598342 // indirect
 )
