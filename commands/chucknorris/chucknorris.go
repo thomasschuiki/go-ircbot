86c8fa74eab19b41bcb73c063f8f8332853e2e5c
Thomas Eitler <thomas.eitler@pm.me>
2020-11-29 20:47:00 +0100

    update chucknorris plugin to use new apirequest function

--- commands/chucknorris/chucknorris.go
+++ commands/chucknorris/chucknorris.go
@@ -1,9 +1,7 @@
 package chucknorris
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"regexp"
 
 	"github.com/go-chat-bot/bot"
@@ -34,11 +32,10 @@ func chucknorris(command *bot.PassiveCmd) (string, error) {
 		url := fmt.Sprintf("%s/jokes/random", baseurl)
 		var joke chuckFact
 
-		resp, err := web.MakeAPIRequest(url, nil)
+		err := web.MakeAPIRequest(url, nil, nil, &joke)
 		if err != nil {
 			return "", err
 		}
-		parseAPIResponse(resp, &joke)
 		return joke.Value, nil
 	}
 
@@ -49,12 +46,4 @@ func init() {
 	bot.RegisterPassiveCommand(
 		"chucknorris", // command
 		chucknorris)   // function
-}
-
-func parseAPIResponse(r *http.Response, v *chuckFact) error {
-	err := json.NewDecoder(r.Body).Decode(&v)
-	if err != nil {
-		return fmt.Errorf("json decode error: %v", err)
-	}
-	return nil
 }
