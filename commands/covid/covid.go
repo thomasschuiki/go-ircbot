175d2340113b1ca631f58a5d7ccd4ea8caf1f3c1
Thomas Schuiki <thomas.schuiki@protonmail.com>
2021-04-08 09:08:18 +0200

    only 7 days in a week

--- commands/covid/covid.go
+++ commands/covid/covid.go
@@ -92,7 +92,7 @@ func covid(command *bot.Cmd) (string, error) {
 	histurl := fmt.Sprintf("%s/covid-19/historical", baseurl)
 	header := make(map[string]string)
 	queryParams := make(map[string]string)
-	lastdays := 8
+	lastdays := 7
 	// analyze parameters if given
 	if len(command.Args) > 0 {
 		countryurl = fmt.Sprintf("%s/%s", countryurl, command.Args[0])
